10b0b8005b5f5a24f09611f373ebfa69049097b8
singleMaltMatt <mattmatte93@gmail.com>
2021-12-13 21:07:35 +0200

    added api key to osm

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 // Creating map options
-var mapOptions = {
+let mapOptions = {
     center: [-26.123377362057052, 28.030939374601676],
     zoom: 10
 }
 
 // Creating a map object
-var map = new L.map('map', mapOptions);
+let map = new L.map('map', mapOptions);
 
 // Creating a Layer object
-var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
 // Adding layer to the map
 map.addLayer(layer);
+
+// Map Control
+let searchControl = L.esri.Geocoding
